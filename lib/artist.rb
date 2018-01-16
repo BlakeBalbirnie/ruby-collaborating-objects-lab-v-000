021e697bf3ae355eaf074c655c7350c2e0c9005b
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-16 15:55:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,41 +1,41 @@
+require 'pry'
 class Artist
-  attr_accessor :name, :songs
+    attr_accessor :name, :songs, :song
+    @@all = []
 
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def songs
-    @songs.each {|song| song}
-  end
-
-  def save
-    if !(@@all.include?(self))
-      @@all << self
+    def initialize(name)
+        @name = name
+        @songs = []
     end
-  end
 
-  def add_song(song)
-    @songs << song
-  end
-
-  def self.find_or_create_by_name(name)
-    if self.all.detect{|artist| artist.name == name}
-      self.all.detect{|artist| artist.name == name}
-    else
-      Artist.new(name)
+    def self.all
+        @@all
     end
-  end
 
-  def print_songs
-    songs.each {|song| puts song.name}
-  end
+    def self.find_or_create_by_name(name)
+       self.find(name) ? self.find(name) : self.create(name)
+    end
 
-  def self.all
-    @@all
-  end
+    def self.find(name)
+        self.all.find {|artist| artist.name == name}
+    end
 
-end
+    def self.create(name)
+        artist = self.new(name)
+        artist.save
+        artist
+    end
+
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
+
+    def save
+        @@all << self
+    end
+
+    def print_songs
+        @songs.each {|song| puts song.name}
+    end
+end 
